6668c148ceea0e63aa43cacd3772bdee51dcd21b
Denis <denis_gulbin@mail.ru>
2023-03-22 17:11:09 +0300

    JS HW

---
+++ script/script.js
@@ -0,0 +1,58 @@
+//  Первое задание 
+//  alert("Hellow, world!");
+
+// Второе задание 
+// alert(`Result 2+2= ${2+2}`);
+
+// Третье задание 
+// let name = "Denis";
+// alert(`Hellow, ${name}`)
+
+// Четвертое задание 
+// let name = prompt("what's your name?", "name");
+// alert(`Hellow, ${name}`);
+
+// Пятое задание 
+// let name = "Denis";
+// function nameUser()
+// {
+//     let message = `Hellow, ${name}`;
+//     alert(message);    
+// }
+// nameUser();
+
+// Шестое задание
+// if(confirm('Are you ready?'))
+// {
+//     alert("We're READY!");
+// }
+// else
+// {
+//     alert("Very sad(((");
+// }
+
+// Седьмое задание 
+// let age = parseInt(prompt("Enter your age", "age"));
+// switch (age) {
+//     case 18:
+//         alert("You are adult!");
+//         break;
+//     case 10:
+//         alert("Go do homework for school!");
+//         break;
+//     case 30:
+//         alert("Go to bad, tommorrow is workday(");
+//         break;
+//     default:
+//         alert("We aren't, what you need to do")
+//         break;
+// }
+
+// Домашнее задание 
+let name = prompt("what's your name?", "name");
+function nameUser()
+{
+    let message = `Hellow, ${name}`;
+    alert(message);    
+}
+nameUser();
